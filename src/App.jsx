ab3967698b0e49e3816d21d50d1278f1c2320acb
Constanza Pérez <constanza.vp.o@gmail.com>
2024-04-08 17:27:38 -0400

    Fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import NavBar from './componentes/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import {Contacto, Home, NotFound} from './layouts'
+import Footer from './componentes/Footer'
 
 function App() {
 
   return (
-    <>
-    <NavBar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/contacto' element={<Contacto/>} />
-      <Route path='*'element={<NotFound/>} />
-    </Routes>
-   
-    </>
+    <div>
+      <NavBar />
+      <Routes >
+        <Route path='/' element={<Home />} />
+        <Route path='/contacto' element={<Contacto/>} />
+        <Route path='*'element={<NotFound/>} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+
+    <div className="card-footer text-muted bg-danger">
+    Fuente: Desafío Latam
+  </div>
+  )
+}
+
+export default Footer

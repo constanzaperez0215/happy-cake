960797431a0e0c90150dcac07fb095fe430213f7
Constanza Pérez <constanza.vp.o@gmail.com>
2024-04-08 16:31:00 -0400

    formato contacto y estilos

--- src/layouts/Contacto.jsx
+++ src/layouts/Contacto.jsx
@@ -1,9 +1,25 @@
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Contacto = () => {
   return (
-    <div>Contacto</div>
-  )
+    <Form className='contacto'>
+        <h3>Cuentanos, ¿en qué te podemos ayudar?</h3>
+        <Form.Group className="correo mb-3">
+            <Form.Label>Correo:</Form.Label>
+            <Form.Control className='cuadroCorreo' type="email" placeholder="name@example.com" />
+        </Form.Group>
+
+        <Form.Group className="descripcion mb-3 ">
+            <Form.Label>Descripción:</Form.Label>
+            <Form.Control className='cuadroDescripcion' type="text" placeholder="Comentanos tus dudas" />
+        </Form.Group>
+
+        <Button variant="danger" type="submit">
+            Enviar
+        </Button>
+  </Form>
+);
 }
 
 export default Contacto
--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,10 +1,10 @@
 const Home = () => {
     return (
-      <>
-          <h1>Bienvenido a <strong>Happy Cake</strong></h1>
-          <p>El lugar de los pasteles felices</p>
-          <img src="https://thumbs.dreamstime.com/z/pastel-de-dibujos-animados-cumplea%C3%B1os-con-velas-y-confeti-tarjeta-felicitaci%C3%B3n-feliz-concepto-evento-celebraci%C3%B3n-ilustraci%C3%B3n-204920564.jpg?w=768" alt="pastel feliz" />
-      </>
+      <div className="principal">
+          <h1 className="bienvenido">Bienvenido a <strong>Happy Cake</strong></h1>
+          <p className="texto">El lugar de los pasteles felices</p>
+          <img className="img" src="https://thumbs.dreamstime.com/z/pastel-de-dibujos-animados-cumplea%C3%B1os-con-velas-y-confeti-tarjeta-felicitaci%C3%B3n-feliz-concepto-evento-celebraci%C3%B3n-ilustraci%C3%B3n-204920564.jpg?w=768" alt="pastel feliz" />
+      </div>
     )
   }
   

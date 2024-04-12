a971e18db4796c874c4f0770830e3b3fcb129fb3
Constanza Pérez <constanza.vp.o@gmail.com>
2024-04-12 10:51:20 -0400

    ruta home

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://constanzaperez0215.github.io/happy-cake/",
+  base: "https://constanzaperez0215.github.io/happy-cake/home",
   plugins: [react()],
 })
